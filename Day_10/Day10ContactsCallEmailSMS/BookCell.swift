b2db961d43e70c5abcfcbd97d35e16f59f061197
Sreejith Thrivikraman <mail.sreejith.23@gmail.com>
2018-03-11 21:09:15 -0400

    Add files via upload

---
+++ Day_10/Day10ContactsCallEmailSMS/BookCell.swift
@@ -0,0 +1,15 @@
+//
+//  BookCell.swift
+//  Day10ContactsCallEmailSMS
+//
+//  Created by Jigisha Patel on 2018-03-02.
+//  Copyright © 2018 JK. All rights reserved.
+//
+
+import UIKit
+
+class BookCell: UICollectionViewCell {
+    
+    @IBOutlet var lblBookTitle: UILabel!
+    @IBOutlet var imgBook: UIImageView!
+}
